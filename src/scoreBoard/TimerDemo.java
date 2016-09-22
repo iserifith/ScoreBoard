bf381cbb3595df29506a304cf895d75e1a4d00b2
Raf <mrafsyam@gmail.com>
2016-09-23 01:55:53 +0800

    Add classes to demo running a method for every second using Timer and TimerTask class

--- src/scoreBoard/TimerDemo.java
+++ src/scoreBoard/TimerDemo.java
@@ -1,20 +1,32 @@
 package scoreBoard;
 
-import java.util.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class TimerDemo {
+
    public static void main(String[] args) {
-   // creating timer task, timer
-   TimerTask tasknew = new TimerScheduleFixedRateDelay();
-   Timer timer = new Timer();
-      
-   // scheduling the task at fixed rate delay
-   timer.scheduleAtFixedRate(tasknew,500,1000);      
-   }
-   // this method performs the task
+
+      // instantiate our TimerTask "extended" class 
+      RunEverySecond tasknew = new RunEverySecond();
+
+      // instantiate a Timer object
+      Timer timer = new Timer();
+         
+      // scheduling the task at fixed rate delay. See http://docs.oracle.com/javase/7/docs/api/java/util/Timer.html#scheduleAtFixedRate(java.util.TimerTask,%20long,%20long)
+      timer.scheduleAtFixedRate(tasknew,0,1000);      
+   } 
+}
+
+/*
+* TimerTask is an "Abstract" class - cannot be instantiated but can only be extended
+* see https://docs.oracle.com/javase/7/docs/api/java/util/TimerTask.html#TimerTask()
+*/
+class RunEverySecond extends TimerTask {
+
    public void run() {
-   System.out.println("working at fixed rate delay");      
-   }    
+      
+      // call the method to change the value of label for timer/clock here
+      System.out.println("Beep! One second");
+   }
 }
